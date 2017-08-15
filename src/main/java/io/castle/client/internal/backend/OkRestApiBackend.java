b1421018cf8362e095544ec02cfd50bf26e57d60
Paweł Cesar Sanjuan Szklarz <p@eiipii.com>
2017-08-15 10:20:52 +0200

    merge params to single json

--- src/main/java/io/castle/client/internal/backend/OkRestApiBackend.java
+++ src/main/java/io/castle/client/internal/backend/OkRestApiBackend.java
@@ -1,6 +1,9 @@
 package io.castle.client.internal.backend;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import io.castle.client.Castle;
 import io.castle.client.internal.config.CastleConfiguration;
 import io.castle.client.internal.model.AuthenticateAction;
@@ -14,6 +17,7 @@ import java.util.concurrent.Future;
 //TODO handle timeout case
 public class OkRestApiBackend implements RestApi {
 
+    private final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private final OkHttpClient client;
     private final CastleGsonModel model;
     private final CastleConfiguration configuration;
@@ -32,16 +36,15 @@ public class OkRestApiBackend implements RestApi {
     }
 
     @Override
-    public int sendTrackRequest(String event, String userId, String contextPayload, String propertiesPayload) {
-        MultipartBody.Builder builder = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("name", event)
-                .addFormDataPart("user_id", userId)
-                .addFormDataPart("context", contextPayload);
+    public int sendTrackRequest(String event, String userId, JsonElement contextPayload, JsonElement propertiesPayload) {
+        JsonObject json = new JsonObject();
+        json.add("name", new JsonPrimitive(event));
+        json.add("user_id", new JsonPrimitive(userId));
+        json.add("context", contextPayload);
         if (propertiesPayload != null) {
-            builder.addFormDataPart("properties", propertiesPayload);
+            json.add("properties", propertiesPayload);
         }
-        MultipartBody body = builder.build();
+        RequestBody body = RequestBody.create(JSON, json.toString());
         Request request = new Request.Builder()
                 .url(track)
                 .post(body)
@@ -60,16 +63,15 @@ public class OkRestApiBackend implements RestApi {
     }
 
     @Override
-    public AuthenticateAction sendAuthenticateSync(String event, String userId, String contextPayload, String propertiesPayload) {
-        MultipartBody.Builder builder = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("name", event)
-                .addFormDataPart("user_id", userId)
-                .addFormDataPart("context", contextPayload);
+    public AuthenticateAction sendAuthenticateSync(String event, String userId, JsonElement contextPayload, JsonElement propertiesPayload) {
+        JsonObject json = new JsonObject();
+        json.add("name", new JsonPrimitive(event));
+        json.add("user_id", new JsonPrimitive(userId));
+        json.add("context", contextPayload);
         if (propertiesPayload != null) {
-            builder.addFormDataPart("properties", propertiesPayload);
+            json.add("properties", propertiesPayload);
         }
-        MultipartBody body = builder.build();
+        RequestBody body = RequestBody.create(JSON, json.toString());
         Request request = new Request.Builder()
                 .url(authenticate)
                 .post(body)
