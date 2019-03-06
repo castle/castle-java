cc028d6160f90925b5b036a45a0038cad88de10f
Sebastian Simson <sebastian@simsons.se>
2019-03-06 14:39:37 +0100

    Expand json serialization message test

--- src/test/java/io/castle/client/model/CastleMessageTest.java
+++ src/test/java/io/castle/client/model/CastleMessageTest.java
@@ -13,12 +13,23 @@ public class CastleMessageTest {
     public void jsonSerialized() {
         // Given
         CastleMessage message = new CastleMessage("event");
+        message.setCreatedAt("2018-01-01");
+        message.setTimestamp("2018-01-01");
+        message.setDeviceToken("1234");
+        message.setReviewId("2345");
+        message.setProperties(ImmutableMap.builder()
+                .put("key", "val")
+                .build());
+        message.setUserId("3456");
+        message.setUserTraits(ImmutableMap.builder()
+                .put("key", "val")
+                .build());
 
         // When
         String payloadJson = model.getGson().toJson(message);
 
         // Then
-        Assertions.assertThat(payloadJson).isEqualTo("{\"event\":\"event\"}");
+        Assertions.assertThat(payloadJson).isEqualTo("{\"created_at\":\"2018-01-01\",\"timestamp\":\"2018-01-01\",\"device_token\":\"1234\",\"event\":\"event\",\"properties\":{\"key\":\"val\"},\"review_id\":\"2345\",\"user_id\":\"3456\",\"user_traits\":{\"key\":\"val\"}}");
     }
 
     @Test
