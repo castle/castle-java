7e7503070ec2214d0a367a30e06a71d559594b59
Sebastian Simson <sebastian@simsons.se>
2019-03-07 14:50:45 +0100

    Include milliseconds in timestamp

--- src/main/java/io/castle/client/internal/utils/Timestamp.java
+++ src/main/java/io/castle/client/internal/utils/Timestamp.java
@@ -7,12 +7,12 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 public class Timestamp {
-    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
+    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", new Locale("en", "US", "POSIX"));
 
     /**
      * Return an ISO 8601 combined date and time string for current date/time
      *
-     * @return String with format "yyyy-MM-dd'T'HH:mm:ss'Z'"
+     * @return String with format "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
      */
     public static String timestamp() {
         return timestamp(new Date());
@@ -22,9 +22,9 @@ public class Timestamp {
      * Return an ISO 8601 combined date and time string for specified date/time
      *
      * @param date Date
-     * @return String with format "yyyy-MM-dd'T'HH:mm:ss'Z'"
+     * @return String with format "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
      */
-    private static String timestamp(Date date) {
+    public static String timestamp(Date date) {
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         return dateFormat.format(date);
     }
