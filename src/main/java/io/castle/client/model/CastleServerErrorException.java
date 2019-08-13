5bcd4d1b8d08d6b5137b0bbda02b99bfb187100c
Sebastian Simson <sebastian@simsons.se>
2019-08-13 13:42:49 +0200

    Add response body to exception class

--- src/main/java/io/castle/client/model/CastleServerErrorException.java
+++ src/main/java/io/castle/client/model/CastleServerErrorException.java
@@ -2,16 +2,24 @@ package io.castle.client.model;
 
 import okhttp3.Response;
 
+import java.io.IOException;
+
 public class CastleServerErrorException extends CastleRuntimeException {
 
     private final int responseCode;
     private final String responseMessage;
+    private String response;
 
     public CastleServerErrorException(Response response) {
         super(response.toString());
 
         this.responseCode = response.code();
         this.responseMessage = response.message();
+        try {
+            this.response = response.body().string();
+        } catch (NullPointerException | IOException | IllegalStateException e) {
+            e.printStackTrace();
+        }
     }
 
     public int getResponseCode() {
@@ -20,5 +28,9 @@ public class CastleServerErrorException extends CastleRuntimeException {
 
     public String getResponseMessage() {
         return responseMessage;
+    }
+
+    public String getResponse() {
+        return response;
     }
 }
