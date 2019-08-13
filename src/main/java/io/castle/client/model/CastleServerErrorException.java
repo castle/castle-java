53fe2f231ddef6558d6cdacb3c606013a3999093
Sebastian Simson <sebastian@simsons.se>
2019-08-13 13:35:47 +0200

    Add fields for response code and message to ServerErrorException

--- src/main/java/io/castle/client/model/CastleServerErrorException.java
+++ src/main/java/io/castle/client/model/CastleServerErrorException.java
@@ -4,7 +4,21 @@ import okhttp3.Response;
 
 public class CastleServerErrorException extends CastleRuntimeException {
 
+    private final int responseCode;
+    private final String responseMessage;
+
     public CastleServerErrorException(Response response) {
         super(response.toString());
+
+        this.responseCode = response.code();
+        this.responseMessage = response.message();
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
     }
 }
