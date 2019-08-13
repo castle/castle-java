3d68b2b7072fad03308fd4c41759d0ca695d814a
Sebastian Simson <sebastian@simsons.se>
2019-08-13 13:33:20 +0200

    Remove unused exception constructors

--- src/main/java/io/castle/client/model/CastleApiInternalServerErrorException.java
+++ src/main/java/io/castle/client/model/CastleApiInternalServerErrorException.java
@@ -4,14 +4,6 @@ import okhttp3.Response;
 
 public class CastleApiInternalServerErrorException extends CastleServerErrorException {
 
-    public CastleApiInternalServerErrorException(Throwable throwable) {
-        super(throwable);
-    }
-
-    public CastleApiInternalServerErrorException(String message) {
-        super(message);
-    }
-
     public CastleApiInternalServerErrorException(Response response) {
         super(response);
     }
--- src/main/java/io/castle/client/model/CastleServerErrorException.java
+++ src/main/java/io/castle/client/model/CastleServerErrorException.java
@@ -4,14 +4,6 @@ import okhttp3.Response;
 
 public class CastleServerErrorException extends CastleRuntimeException {
 
-    public CastleServerErrorException(Throwable throwable) {
-        super(throwable);
-    }
-
-    public CastleServerErrorException(String message) {
-        super(message);
-    }
-
     public CastleServerErrorException(Response response) {
         super(response.toString());
     }
