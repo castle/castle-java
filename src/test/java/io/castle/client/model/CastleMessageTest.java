07c630c34f712fcb821fb3e5e669c6227e76b1a9
marysieek <maria.korlotian@castle.io>
2021-03-29 16:12:11 +0200

    Fix castle message test

--- src/test/java/io/castle/client/model/CastleMessageTest.java
+++ src/test/java/io/castle/client/model/CastleMessageTest.java
@@ -75,7 +75,7 @@ public class CastleMessageTest {
         String payloadJson = model.getGson().toJson(message);
 
         // Then
-        Assertions.assertThat(payloadJson).isEqualTo("{\"created_at\":\"2018-01-01\",\"timestamp\":\"2018-01-01\",\"device_token\":\"1234\",\"event\":\"event\",\"properties\":{\"key\":\"val\"},\"review_id\":\"2345\",\"user_id\":\"3456\",\"user_traits\":{\"key\":\"val\"}}");
+        Assertions.assertThat(payloadJson).isEqualTo("{\"created_at\":\"2018-01-01\",\"timestamp\":\"2018-01-01\",\"device_token\":\"1234\",\"event\":\"event\",\"status\":\"$succeeded\",\"email\":\"test@example.com\",\"properties\":{\"key\":\"val\"},\"review_id\":\"2345\",\"user_id\":\"3456\",\"user_traits\":{\"key\":\"val\"}}");
     }
 
     @Test
