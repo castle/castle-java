d5b689b3d26d73a372bf8194e1de6eefe4a4f827
Sebastian Simson <sebastian@simsons.se>
2022-05-20 09:32:33 +0200

    Add not found exception (#118)

--- src/main/java/io/castle/client/internal/utils/OkHttpExceptionUtil.java
+++ src/main/java/io/castle/client/internal/utils/OkHttpExceptionUtil.java
@@ -18,20 +18,22 @@ public class OkHttpExceptionUtil {
 
     public static void handle(Response response) throws CastleServerErrorException {
         if (!response.isSuccessful() && !response.isRedirect()) {
-            if (response.code() == 500) {
-                throw new CastleApiInternalServerErrorException(response);
-            }
-            if (response.code() == 422) {
-                try {
-                    String body = response.peekBody(Long.MAX_VALUE).string();
-                    JsonObject json = (JsonObject) JsonParser.parseString(body);
-                    String type = json.get("type").getAsString();
+            switch (response.code()) {
+                case 500:
+                    throw new CastleApiInternalServerErrorException(response);
+                case 422:
+                    try {
+                        String body = response.peekBody(Long.MAX_VALUE).string();
+                        JsonObject json = (JsonObject) JsonParser.parseString(body);
+                        String type = json.get("type").getAsString();
 
-                    if (type.equals("invalid_request_token")) {
-                        throw new CastleApiInvalidRequestTokenException(response);
-                    }
-                } catch (IOException | JsonSyntaxException | JsonIOException | IllegalStateException ignored) {}
-                throw new CastleApiInvalidParametersException(response);
+                        if (type.equals("invalid_request_token")) {
+                            throw new CastleApiInvalidRequestTokenException(response);
+                        }
+                    } catch (IOException | JsonSyntaxException | JsonIOException | IllegalStateException ignored) {}
+                    throw new CastleApiInvalidParametersException(response);
+                case 404:
+                    throw new CastleApiNotFoundException(response);
             }
             throw new CastleServerErrorException(response);
         }
